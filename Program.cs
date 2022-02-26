4817656fa5c0334e10b51dca602ccef48b305ec2
jose felipe da silva santos <josefelipesilvasantos874@gmail.com>
2022-02-25 22:39:03 -0300

    finalizar o exercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("peruibe");
+Console.WriteLine("ubatuba");
+Console.WriteLine("caraguatatuba");
+Console.WriteLine("ilhabela");
+Console.WriteLine("sao sebastiao");
+Console.WriteLine("bertioga");
+Console.WriteLine("guaruja");
+Console.WriteLine("santos");
+Console.WriteLine("sao vicente");
+Console.WriteLine("praia grande");
+Console.WriteLine("mongagua");
+Console.WriteLine("itanhaem");
+Console.WriteLine("iguape");
+Console.WriteLine("ilha comprida");
+Console.WriteLine("cananeia"); 
